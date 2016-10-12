afe1b2e02c3dde8294cd168e277511a7df8306a4
Ignacio <ignacio.lesci@gmail.com>
2016-10-11 21:44:07 -0300

    creo menu contextual y menu en la toolbar aun sin acciones no llegé

---
+++ app/src/main/java/com/example/ignacio/lab03c2016/CrearOfertaActivity.java
@@ -0,0 +1,13 @@
+package com.example.ignacio.lab03c2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearOfertaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_oferta);
+    }
+}
--- app/src/main/java/com/example/ignacio/lab03c2016/MainActivity.java
+++ app/src/main/java/com/example/ignacio/lab03c2016/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.ignacio.lab03c2016;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -22,6 +28,36 @@ public class MainActivity extends AppCompatActivity {
         listaEmpleos = (ListView) findViewById(R.id.empleosListView);
         listaTrabajosAdaptador = new Adaptador (this,mTrabajo.TRABAJOS_MOCK);
         listaEmpleos.setAdapter(listaTrabajosAdaptador);
+
+
+        registerForContextMenu(listaEmpleos);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.mimenuprincipal,menu);
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_ctx,menu);
+        menu.setHeaderTitle("Acciones");
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.idMenu1:
+                Intent intent = new Intent(this,CrearOfertaActivity.class);
+                startActivityForResult(intent,0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
 
     }
 }
